8c64a6c24873bc76a6fe720773d1fe653e144a12
aakaashkapoor <aakaash1308@gmail.com>
2019-03-20 01:58:31 -0700

    Added templates(html) and static(css), as well as empty loading page

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
-# Pointing to the first page
+# Pointing to the loading page
+@app.route("/loading")
+def loading():
+	return "<h1>Starting an empty flask project</h1>"
+
+# Pointing to the main page
 @app.route("/")
+@app.route("/main")
 def main():
-	return "<h1>Started an empty flask project</h1>"
+	return render_template('main.html')
 
 
 # Allows us to run and refresh on debug mode
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
+# REFRENCES
+# https://www.youtube.com/watch?v=MwZwr5Tvyxo
+# 17 17 17
